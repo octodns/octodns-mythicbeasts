99b65df14277c74d02a3d8b8d69276388fcaa0e3
Ross McFarland <rwmcfa1@gmail.com>
2022-01-12 07:46:40 -0800

    Import skeleton

---
+++ tests.py
@@ -0,0 +1,12 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_mythicbeasts import MythicBeastsProvider
+
+
+class TestMythicBeastsProvider(TestCase):
+    # TODO: test provider
+    MythicBeastsProvider
